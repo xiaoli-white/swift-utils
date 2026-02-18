1e69faa1f0fc91982fc2ad645a36a552836f34f7
xiaoli-white <maxljh0216@yeah.net>
2026-02-19 00:31:40 +0800

    ✨ feat: add Equatable and Hashable to Unmanaged

--- Sources/SwiftUtils/SwiftUtils.swift
+++ Sources/SwiftUtils/SwiftUtils.swift
@@ -1,4 +1,11 @@
-extension Unmanaged {
+extension Unmanaged:@retroactive Equatable, @retroactive Hashable {
+    public static func == (lhs: Unmanaged, rhs: Unmanaged) -> Bool {
+        lhs.toOpaque() == rhs.toOpaque()
+    }
+    
+    public func hash(into hasher: inout Hasher) {
+        hasher.combine(toOpaque())
+    }
     public func to<T>() -> Unmanaged<T> {
         Unmanaged<T>.fromOpaque(self.toOpaque())
     }
