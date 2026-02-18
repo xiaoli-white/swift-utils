f6ed71b7264df402c65f4495f90862b9aa9e7916
xiaoli-white <maxljh0216@yeah.net>
2026-02-18 23:55:24 +0800

    🔧 build: change SwiftUtils library type to static

--- Package.swift
+++ Package.swift
@@ -9,7 +9,6 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "SwiftUtils",
-            type: .dynamic,
             targets: ["SwiftUtils"]
         ),
     ],
